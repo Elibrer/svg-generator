a899a47b1a45b984ac7a3d075d92fdd0b8eb365d
Elibrer <eli.brer@gmail.com>
2023-04-20 16:05:52 +1000

    SVG Generator working to some degree, needs tweaking. Working out tests for jest.

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,9 +1,9 @@
-const Logo = require('../lib/logo.js');
+const { Square, Circle, Triangle } = require('../lib/shapes.js');
 
 describe('Logo', () => {
   test(`Shouldn't throw error if parent renderLogo function is called`, () => {
-    const logo = new Logo();
-    const err = new Error('Child class must implement a render() method.')
-    expect(logo.renderLogo).toThrow(err);
+    const logo = new Square();
+    const err = new Error('Child class must implement a renderShape() method.')
+    expect(logo.renderShape).toThrow(err);
   });
 });
